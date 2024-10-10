3cc34072e63fa3b4bb2ea1f02a0d657587312bbe
Daniel Figuerêdo <danielfigueredo.dev@gmail.com>
2024-10-10 10:38:46 -0300

    Implementação do carrossel

---
+++ app.js
@@ -0,0 +1,19 @@
+
+let radio = document.querySelector('.manual-btn');
+let cont = 1;
+
+document.getElementById('radio1').checked =true;
+
+setInterval(() =>{
+    proximaImg()
+}, 5000);
+
+function proximaImg(){
+    cont++
+
+    if(cont > 3){
+        cont = 1;
+    }
+
+    document.getElementById('radio'+cont).checked =true;
+}
